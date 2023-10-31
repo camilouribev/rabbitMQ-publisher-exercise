bfbf02c19d0329a4efda35f90346cf47704ae526
camilo.uribev <camilo.uribev@sofka.com.co>
2023-10-31 12:21:24 -0700

    Created sender and DTO

---
+++ src/main/java/com/demo/mailbuildingpublisher/dto/BuildingMailDTO.java
@@ -0,0 +1,9 @@
+package com.demo.mailbuildingpublisher.dto;
+
+import lombok.Data;
+
+@Data
+public class BuildingMailDTO {
+    private String message;
+
+}
---
+++ src/main/java/com/demo/mailbuildingpublisher/infrastructure/mqsender/RabbitMQSender.java
@@ -0,0 +1,28 @@
+package com.demo.mailbuildingpublisher.infrastructure.mqsender;
+
+import com.demo.mailbuildingpublisher.dto.BuildingMailDTO;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+
+@Service
+public class RabbitMQSender {
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    @Value("${broker.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${broker.rabbitmq.routingkey}")
+    private String routingKey;
+
+
+    public Mono<Void> send(BuildingMailDTO buildingMailDTO){
+        rabbitTemplate.convertAndSend(exchange, routingKey, buildingMailDTO);
+
+        return Mono.empty();
+    }
+}
