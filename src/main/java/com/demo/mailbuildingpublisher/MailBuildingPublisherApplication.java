780d4835d8ef29b087a7b4f56bfc21ef7caf9880
camilo.uribev <camilo.uribev@sofka.com.co>
2023-10-31 12:08:19 -0700

    Project initialization

---
+++ src/main/java/com/demo/mailbuildingpublisher/MailBuildingPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mailbuildingpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailBuildingPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailBuildingPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/mailbuildingpublisher/MailBuildingPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.mailbuildingpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailBuildingPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
